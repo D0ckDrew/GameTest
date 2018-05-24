ab32c206262bb4500c8d4225487679442491e3f8
D0ckDrew <D0ckDrew@yandex.ru>
2018-05-24 18:41:07 +0300

    че

---
+++ Cyl.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cyl : MonoBehaviour
+{
+    public GameObject cube;
+    public float speed, tilt;
+
+    void Update()
+    {
+        transform.RotateAround(cube.transform.position,new Vector3(0,1,0), 1/(tilt/7));
+    }
+}
+
